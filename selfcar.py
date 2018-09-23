70aebd71d4f4e5a193a4fa4b592849c00693d040
otoshuki <guining.pertin.iitg@gmail.com>
2018-09-24 02:36:27 +0530

    First Commit - added important lines

---
+++ selfcar.py
@@ -0,0 +1,45 @@
+"""
+1. Build Car Model
+2. Find the track boundaries
+?. Move the car
+"""
+#Import required libraries
+import cv2
+import numpy as nn
+
+#Detect particular colored balloons
+def detect():
+    #argument - color
+    print('not done yet')
+    #returns coordinates [x,y]
+
+
+#In case balloons of same color
+def dist():
+    #arguments - coordinates x,y
+    print('not done yet')
+    #returns distance
+
+
+
+#Go through balloons in sequence
+def seq():
+    #argument - colors available
+    print('not done yet')
+
+
+
+#Algorithm to move arm to required coordinates
+def move():
+    #arguments - coordinates x,y
+    print('not done yet')
+
+
+#Main function
+def main():
+    print('Not completed yet')
+
+
+#Run the program
+if __name__ == '__main__':
+    main()
